c9f9437621b8230c77c9a3c1f5806e3a8c4e7d23
Kshitij kumar <kshitij@Kshitijs-MBP.eau.wi.charter.com>
2019-11-01 15:18:32 -0500

    Drafting page components added

--- DraftPageReact/src/CurrentDraft.js
+++ DraftPageReact/src/CurrentDraft.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 class CurrentDraft extends React.Component {
     render() {
         return(
-            <div className="App">
+            <div>
+                {getPlayers(["Ronaldo", "Messi"], 4)}
             </div>
         )
     }
 
-    currentDraft(){
-
+    getPlayers(currentList, numOfPlayers){
+        var tempList = [];
+        var count = 0;
+        for(const player of currentList){
+            if(count === numOfPlayers){
+                count = 0;
+            }
+            count++;
+            render(
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Player {count}</Card.Title>
+                        <Card.Subtitle>{player.name}</Card.Subtitle>
+                    </Card.Body>
+                </Card>
+            )
+        }
+        return tempList;
     }
 }
 
