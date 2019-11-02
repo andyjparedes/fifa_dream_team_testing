3c27abab0105ae79c4b79f966e14f69a08ee4d98
andyjparedes <aparedes@wisc.edu>
2019-11-01 19:34:04 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
+// Allows user to input the team names for each participating team
 function inputTeamNames() {
+    //clear the space
+    //this is necessary if the user changes their mind on number of teams
     document.getElementById("teamnames").innerHTML = "";
+
+    // create the correct number of input boxes as requested by the user
     for (let i = 0; i < document.getElementById("drop1").value; i++) {
         let teamnames = document.getElementById("teamnames");
+
+        //create a leading text to describe which team it is
         let text = document.createTextNode("Team " + (i+1));
+
+        //create the actual input boxes
         let x = document.createElement("INPUT");
         x.setAttribute("id", "team" + (i+1));
         x.setAttribute("type", "text");
+
+        //add the elements to the page
         teamnames.appendChild(text);
         teamnames.appendChild(x);
     }
 }
 
+// the result of clicking the Draft Page Button
+// stores the user input into local storage
 function goToDraftPage() {
     localStorage.setItem("index_numteams", document.getElementById("drop1").value);
     for (let i = 0; i < document.getElementById("drop1").value; i++) {
@@ -22,9 +35,12 @@ function goToDraftPage() {
     localStorage.setItem("index_roundtrans", document.getElementById("drop5").value);
     localStorage.setItem("index_version", document.getElementById("drop6").value);
     
+    //redirect to the Resulting Page
+    //should redirect to the Drafting Page, which will be changed when the issue is solved
     window.location = "resultpage.html";
 }
 
+// initialize the listeners for the page
 window.onload = function init() {
     this.document.getElementById("draftpage").addEventListener("click", goToDraftPage);
     this.document.getElementById("drop1").addEventListener("change", inputTeamNames);
